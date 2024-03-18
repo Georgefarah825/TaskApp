6093cc25189c48d03aa0fd25c3ef3290c132da49
Georgefarah825 <162799743+Georgefarah825@users.noreply.github.com>
2024-03-18 10:54:09 +0300

    Create bloc_exports.dart

---
+++ lib/bloc/bloc_exports.dart
@@ -0,0 +1,6 @@
+export 'package:taskapp/blocs/bloc/TaskBloc/task_bloc.dart';
+export 'package:flutter_bloc/flutter_bloc.dart';
+export 'package:hydrated_bloc/hydrated_bloc.dart';
+export 'package:taskapp/models/task_model.dart';
+
+export 'SwitchBloc/theme_bloc_bloc.dart';
