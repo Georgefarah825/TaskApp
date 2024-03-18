5474c337ef9f1a6a5f55368de37a733de26ba90e
Georgefarah825 <162799743+Georgefarah825@users.noreply.github.com>
2024-03-18 10:50:51 +0300

    Create app_theme.dart

---
+++ lib/servcies/app_theme.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+enum AppTheme { lightTheme, darkTheme }
+
+class AppThemes {
+  static final appThemeData = {
+    AppTheme.darkTheme: ThemeData(
+      drawerTheme: DrawerThemeData(
+        backgroundColor: Colors.blueGrey,
+      ),
+      primarySwatch: Colors.grey,
+      primaryColor: Colors.black,
+      brightness: Brightness.dark,
+      backgroundColor: const Color(0xFF212121),
+      dividerColor: Colors.black54,
+      floatingActionButtonTheme: const FloatingActionButtonThemeData(
+        backgroundColor: Colors.white,
+      ),
+      textButtonTheme: TextButtonThemeData(
+        style: ButtonStyle(
+          foregroundColor: MaterialStateProperty.all(Colors.white),
+        ),
+      ),
+      textTheme: const TextTheme(
+        subtitle1: TextStyle(color: Colors.white),
+      ),
+      bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+          backgroundColor: Colors.grey, unselectedItemColor: Colors.white),
+    ),
+
+    //
+    //
+
+    AppTheme.lightTheme: ThemeData(
+      appBarTheme: const AppBarTheme(
+          iconTheme: IconThemeData(color: Colors.white),
+          titleTextStyle: TextStyle(color: Colors.white, fontSize: 20),
+          textTheme: const TextTheme(
+            subtitle1: TextStyle(color: Colors.black),
+          )),
+      drawerTheme: const DrawerThemeData(
+        backgroundColor: Colors.cyan,
+      ),
+      primarySwatch: Colors.cyan,
+      primaryColor: Colors.white,
+      brightness: Brightness.light,
+      backgroundColor: const Color(0xFFE5E5E5),
+      dividerColor: const Color(0xff757575),
+      floatingActionButtonTheme: const FloatingActionButtonThemeData(
+        backgroundColor: Colors.cyan,
+        foregroundColor: Colors.white,
+      ),
+      textButtonTheme: TextButtonThemeData(
+        style: ButtonStyle(
+          foregroundColor: MaterialStateProperty.all(Colors.black),
+        ),
+      ),
+      textTheme: const TextTheme(
+        subtitle1: TextStyle(color: Colors.black),
+      ),
+      bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+          backgroundColor: Colors.cyan,
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.white54),
+    ),
+  };
+}
